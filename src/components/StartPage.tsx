f93294cd2859b60d58f2cb354480b3c0d7c844bd
nanidam <nani.dam@medieinstitutet.se>
2023-09-05 19:41:39 +0200

    :sparkles: added customer services

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -1,17 +1,13 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { IRestaurant } from "../models/IRestaurant";
 import { createRestaurant, getRestaurant } from "../services/restaurantService";
 
 export function StartPage() {
-    const[restaurant, setRestaurant] = useState<[]>([]);
+  const [restaurant, setRestaurant] = useState<[]>([]);
 
-    useEffect(() => {
-        const restaurantId = createRestaurant();      
-    });
+  useEffect(() => {
+    const restaurantId = createRestaurant();
+  });
 
-    return(
-        <>
-            Startpage
-        </>
-    )
+  return <>Startpage</>;
 }
