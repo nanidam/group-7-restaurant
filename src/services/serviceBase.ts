02ade5d1f5c7b76e0aa31b0238b9d8b121479a8e
nanidam <nani.dam@medieinstitutet.se>
2023-09-05 20:17:13 +0200

    :sparkles: added post

--- src/services/serviceBase.ts
+++ src/services/serviceBase.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+//TODO: endpoints instead of url?
 export async function get<T>(url: string) {
   const response = await axios.get<T>(url);
   return response.data;
 }
 
-/*export async function post<T>(url: string) {
-
-}*/
+export async function post<T>(url: string, data: T) {
+  const response = await axios.post<T>(url, data);
+  return response.data;
+}
