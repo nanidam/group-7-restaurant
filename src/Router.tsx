7d5bda3e891f31cf5cb2a3e9b6d2dc2ae5534122
nanidam <nani.dam@medieinstitutet.se>
2023-09-06 14:49:57 +0200

    ➕ added admin router

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,26 +4,31 @@ import { StartPage } from "./components/StartPage";
 import { NotFound } from "./components/NotFound";
 import { BookingPage } from "./components/BookingPage";
 import { ContactPage } from "./components/ContactPage";
+import { AdminPage } from "./components/AdminPage";
 
 export const Router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <LayoutStructure></LayoutStructure>,
+    errorElement: <NotFound></NotFound>,
+    children: [
+      {
         path: "/",
-        element: <LayoutStructure></LayoutStructure>,
-        errorElement: <NotFound></NotFound>,
-        children: [
-            {
-                path: "/",
-                element: <StartPage></StartPage>,
-                index: true
-            },
-            {
-                path: "/booking",
-                element: <BookingPage></BookingPage>
-            },
-            {
-                path: "/contact",
-                element: <ContactPage></ContactPage>
-            },
-        ]
-    }
+        element: <StartPage></StartPage>,
+        index: true,
+      },
+      {
+        path: "/booking",
+        element: <BookingPage></BookingPage>,
+      },
+      {
+        path: "/contact",
+        element: <ContactPage></ContactPage>,
+      },
+      {
+        path: "/admin",
+        element: <AdminPage></AdminPage>,
+      },
+    ],
+  },
 ]);
--- src/components/FooterBar.tsx
+++ src/components/FooterBar.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export function FooterBar() {
   return (
     <>
-      <button className="admin-btn">Admin</button>
+      <Link to="/admin">
+        <button className="admin-btn">Admin</button>
+      </Link>
       <p>Made by: Emilia, Nani, Filip</p>
     </>
   );
