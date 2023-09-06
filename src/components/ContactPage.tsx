907dcbe2594ebff97908f1cdc2ac53179bbacfa9
nanidam <nani.dam@medieinstitutet.se>
2023-09-06 14:10:10 +0200

    ✨ added contact info from ls

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -1,7 +1,28 @@
+import { IRestaurant } from "../models/IRestaurant";
+import "../style/_contactPage.scss";
+
 export function ContactPage() {
-    return(
-        <>
-            ContactPage
-        </>
-    )
+  const localRestaurantData = localStorage.getItem("restaurant");
+  let localRestaurantJSON: IRestaurant[] | null = null;
+
+  if (localRestaurantData) {
+    localRestaurantJSON = JSON.parse(localRestaurantData);
+  }
+
+  return (
+    <>
+      <div className="wrapper">
+        {localRestaurantJSON && (
+          <>
+            <p>{localRestaurantJSON[0].name}</p>
+            <p>{localRestaurantJSON[0].address}</p>
+            <p>{localRestaurantJSON[0].zip}</p>
+            <p>{localRestaurantJSON[0].city}</p>
+            <p>Open: 17:00-01:00</p>
+            <p>Phone: +39 123 12 12</p>
+          </>
+        )}
+      </div>
+    </>
+  );
 }
