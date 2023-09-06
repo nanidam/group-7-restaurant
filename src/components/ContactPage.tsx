9173d333b8224fcb34f18d9aa1ad5d1834767303
nanidam <nani.dam@medieinstitutet.se>
2023-09-06 14:33:29 +0200

    :art: contact info styling

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -1,28 +1,31 @@
 import { IRestaurant } from "../models/IRestaurant";
+import { useEffect, useState } from "react";
 import "../style/_contactPage.scss";
 
 export function ContactPage() {
-  const localRestaurantData = localStorage.getItem("restaurant");
-  let localRestaurantJSON: IRestaurant[] | null = null;
+  const [localRestaurantJSON, setLocalRestaurantJSON] = useState<
+    IRestaurant[] | null
+  >(null);
 
-  if (localRestaurantData) {
-    localRestaurantJSON = JSON.parse(localRestaurantData);
-  }
+  useEffect(() => {
+    const localRestaurantData = localStorage.getItem("restaurant");
+    if (localRestaurantData) {
+      setLocalRestaurantJSON(JSON.parse(localRestaurantData));
+    }
+  }, []);
 
   return (
-    <>
-      <div className="wrapper">
-        {localRestaurantJSON && (
-          <>
-            <p>{localRestaurantJSON[0].name}</p>
-            <p>{localRestaurantJSON[0].address}</p>
-            <p>{localRestaurantJSON[0].zip}</p>
-            <p>{localRestaurantJSON[0].city}</p>
-            <p>Open: 17:00-01:00</p>
-            <p>Phone: +39 123 12 12</p>
-          </>
-        )}
-      </div>
-    </>
+    <div className="wrapper">
+      {localRestaurantJSON && (
+        <>
+          <p className="restaurantInfo">{localRestaurantJSON[0].address}</p>
+          <p className="restaurantInfo">
+            {localRestaurantJSON[0].zip} {localRestaurantJSON[0].city}
+          </p>
+          <p className="restaurantInfo">Open: 17:00-01:00</p>
+          <p className="restaurantInfo">Phone: +39 123 12 12</p>
+        </>
+      )}
+    </div>
   );
 }
