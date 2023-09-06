01ac7d46d0f3f9d2c4ec493593461b5c21b1c597
nanidam <nani.dam@medieinstitutet.se>
2023-09-06 15:20:15 +0200

    📚 edit footer info

--- src/components/FooterBar.tsx
+++ src/components/FooterBar.tsx
@@ -1,15 +1,33 @@
 import { Link } from "react-router-dom";
 import "../style/FooterBar.scss";
+import { IRestaurant } from "../models/IRestaurant";
+import { useEffect, useState } from "react";
 
 export function FooterBar() {
+  const [localRestaurantJSON, setLocalRestaurantJSON] = useState<
+    IRestaurant[] | null
+  >(null);
+
+  useEffect(() => {
+    const localRestaurantData = localStorage.getItem("restaurant");
+    if (localRestaurantData) {
+      setLocalRestaurantJSON(JSON.parse(localRestaurantData));
+    }
+  }, []);
+
   return (
     <>
-      <div className="footer-wrapper">
-        <Link to="/admin">
-          <button className="admin-btn">Admin</button>
-        </Link>
-        <p className="placeholder-text">Made by: Emilia, Nani, Filip </p>
-      </div>
+      {localRestaurantJSON && (
+        <div className="footer-wrapper">
+          <Link to="/admin">
+            <button className="admin-btn">Admin</button>
+          </Link>
+          <p className="placeholder-text">
+            {localRestaurantJSON[0].name} <br />
+            Made by: Emilia, Nani, Filip
+          </p>
+        </div>
+      )}
     </>
   );
 }
