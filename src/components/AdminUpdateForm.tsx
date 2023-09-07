8d521aafc9a37e268617afad2202ca18457562b2
nanidam <nani.dam@medieinstitutet.se>
2023-09-07 11:21:13 +0200

    added updateform

--- src/components/AdminUpdateForm.tsx
+++ src/components/AdminUpdateForm.tsx
@@ -1,3 +1,24 @@
+import "../style/AdminUpdateForm.scss";
+
 export function AdminUpdateForm() {
-  return <>test</>;
+  return (
+    <form>
+      <label htmlFor="restaurantId">Restaurant ID</label>
+      <input type="text" id="restaurantId" />
+
+      <label htmlFor="date">Date</label>
+      <input type="date" id="date" />
+
+      <label htmlFor="time">Time</label>
+      <input type="time" id="time" />
+
+      <label htmlFor="numberOfGuests">Number of Guests</label>
+      <input type="number" id="numberOfGuests" />
+
+      <label htmlFor="customerId">Customer ID</label>
+      <input type="text" id="customerId" />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
