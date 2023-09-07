73cb0196b5ba5ad21f6a042d17da2b0d0423f780
Emilia <emiliamassings@gmail.com>
2023-09-07 15:21:19 +0200

    🚧 added booking form

--- src/components/BookingPage.tsx
+++ src/components/BookingPage.tsx
@@ -1,7 +1,20 @@
+import "../style/bookingPage.scss";
+
 export function BookingPage() {
     return(
         <>
-            BookingPage
+            <section>
+                <h3>Book a table:</h3>
+                <div className="formContainer">
+                    <form>
+                        <label htmlFor="date">Date</label>
+                        <input type="date" name="date"></input>
+                        <label htmlFor="quantity">Guests</label>
+                        <input type="number" name="quantity" min="1" max="6"></input>
+                        <button>Search</button>
+                    </form>
+                </div>
+            </section>
         </>
     )
 }
