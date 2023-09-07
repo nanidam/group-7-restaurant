cec6bc558290ed7c2c8389cbf8b60c1ae6b5b814
nanidam <nani.dam@medieinstitutet.se>
2023-09-07 11:31:16 +0200

    rearrange form

--- src/components/AdminUpdateForm.tsx
+++ src/components/AdminUpdateForm.tsx
@@ -2,9 +2,12 @@ import "../style/AdminUpdateForm.scss";
 
 export function AdminUpdateForm() {
   return (
-    <form>
-      <label htmlFor="restaurantId">Restaurant ID</label>
-      <input type="text" id="restaurantId" />
+    <form className="update-form">
+      <label htmlFor="customerId">Customer ID</label>
+      <input type="text" id="customerId" />
+
+      <label htmlFor="numberOfGuests">Number of Guests</label>
+      <input type="number" id="numberOfGuests" />
 
       <label htmlFor="date">Date</label>
       <input type="date" id="date" />
@@ -12,13 +15,8 @@ export function AdminUpdateForm() {
       <label htmlFor="time">Time</label>
       <input type="time" id="time" />
 
-      <label htmlFor="numberOfGuests">Number of Guests</label>
-      <input type="number" id="numberOfGuests" />
-
-      <label htmlFor="customerId">Customer ID</label>
-      <input type="text" id="customerId" />
-
       <button type="submit">Submit</button>
+      <button>Cancel</button>
     </form>
   );
 }
